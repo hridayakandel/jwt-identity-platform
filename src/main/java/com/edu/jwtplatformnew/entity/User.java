a18b1320c6e20131557115c7c77b6a79c74b5f8e
hridayakandel <hridayakandel@gmail.com>
2023-03-23 17:25:29 -0500

    azure

--- src/main/java/com/edu/jwtplatformnew/entity/User.java
+++ src/main/java/com/edu/jwtplatformnew/entity/User.java
@@ -1,6 +1,10 @@
 package com.edu.jwtplatformnew.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import javax.persistence.*;
 import java.util.List;
@@ -8,14 +12,20 @@ import java.util.List;
 @Entity
 @Data
 @Table(name = "Users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
+    @GeneratedValue
     private int id;
+
     private String email;
     private String password;
     private String firstName;
     private String lastName;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable
+    @ManyToMany
+    @JoinTable(joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private List<Role> roles;
 }
